8763ee65c4dbc4398d2d2cb12431cddc4de68010
mikhailzakharov <zakharov3010@yandex.ru>
2023-04-18 22:17:52 +0400

    feat: add second project, update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rust_workbook
 my notes and examples when learning rust
+
+| ID | Name            |
+|----|-----------------|
+| 1  | 'Hello world'   |
+| 2  | 'Guessing game' |
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::io;
+
 fn main() {
     // Project "Hello, world!"
-    println!("Hello, world!")
+    println!("Hello, world!");
+
+    // Project "Guessing game"
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
