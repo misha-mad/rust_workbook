f3b014212070bf4c63e973ff281d92fc0ff475bb
mikhailzakharov <zakharov3010@yandex.ru>
2023-04-19 21:44:43 +0400

    feat: final second project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     // Project "Hello, world!"
@@ -6,12 +8,30 @@ fn main() {
 
     // Project "Guessing game"
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
