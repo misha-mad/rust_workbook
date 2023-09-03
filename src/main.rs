0a6fc32e09684453e9012f5f8f16a526041800dc
misha-mad <zakharov3010@yandex.ru>
2023-09-03 22:25:23 +0400

    Add Fibonacci number generator function

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ fn main() {
         1 => hello(),
         2 => guessing_game(),
         3 => fahrenheit_to_celsius(),
+        4 => fibonacci(),
         _ => println!("There is no such project."),
     }
 
@@ -79,5 +80,37 @@ fn main() {
 
         let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
         println!("{fahrenheit} degrees Fahrenheit is equal to {celsius} degrees Celsius.");
+    }
+
+    // Project "Fibonacci numbers"
+    fn fibonacci() {
+        println!("Generate Fibonacci numbers.");
+        println!("Please input the value of 'n'.");
+
+        let mut n = String::new();
+        io::stdin().read_line(&mut n).expect("Failed to read line");
+
+        let n: usize = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a valid number.");
+                return;
+            }
+        };
+
+        let mut fibonacci_numbers: Vec<u32> = vec![0, 1];
+
+        for i in 2..n {
+            let next = &fibonacci_numbers[&i - 1] + &fibonacci_numbers[&i - 2];
+            fibonacci_numbers.push(next);
+        }
+
+        let fibonacci_str: Vec<String> = fibonacci_numbers
+            .iter()
+            .map(|num| num.to_string())
+            .collect();
+
+        let result = fibonacci_str.join(", ");
+        println!("Fibonacci sequence: {:?}", result);
     }
 }
