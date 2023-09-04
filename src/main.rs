38b227feef7ad1badb920b14979f40a3378f4c5e
Mikhail Zakharov <zakharov3010@yandex.ru>
2023-09-04 16:38:16 +0400

    Refactor 'Hello, world!' to accept user input and provide personalized greeting

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,30 @@ fn main() {
 
     // Project "Hello, world!"
     fn hello() {
-        let mut string = String::from("   Hello");
-        string.push_str(", world");
-        string.push('!');
-        println!("{}", string.trim());
+        println!("Please input your name.");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let name = if input.trim().len() > 0 {
+            input.trim().to_owned()
+        } else {
+            "world".to_owned()
+        };
+
+        fn print_hello(name: &String) {
+            let mut string = String::from("Hello, ");
+            string.push_str(name);
+            string.push('!');
+            println!("{}", string);
+        }
+
+        print_hello(&name);
+
+        // After passing the name reference to the function, the name variable is not moved and remains available
+        println!("Have a good day, {}.", name)
     }
 
     // Project "Guessing game"
